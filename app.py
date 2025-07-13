cb8c8d9a8867a19347d9fd86bfec314da643be16
Manas Shende <manasshende2004@gmail.com>
2025-07-13 17:00:30 +0530

    added frontend: cors fix

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Allow your frontend origin
+    allow_origins=["http://localhost:5173", "https://insurance-premium-prediction-api-a.vercel.app/"],  # Allow your frontend origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
